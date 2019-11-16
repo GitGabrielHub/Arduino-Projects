9aafd42ff4432c01daec7f7137dabe4c0e18cdbb
Gabriel <39504004+GitGabrielHub@users.noreply.github.com>
2019-11-16 10:54:10 -0300

    Add files via upload

---
+++ IRsendWithArduino/IRsendWithArduino.ino
@@ -0,0 +1,48 @@
+#include <IRremote.h>
+
+IRsend irsend;
+
+byte PIN_ECHO = 6;
+byte PIN_TRIG = 5;
+
+unsigned long time;
+double dist;
+
+boolean condictionOn  = false;
+boolean condictionOff = true;
+
+void setup(){
+  pinMode(PIN_TRIG,OUTPUT);
+  pinMode(PIN_ECHO,INPUT);
+  
+  Serial.begin(9600);
+
+  digitalWrite(PIN_TRIG,LOW);
+}
+
+void loop() {
+
+  digitalWrite(PIN_TRIG,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG,LOW);
+
+  time = pulseIn(PIN_ECHO,HIGH);
+
+  dist = time/58;
+
+  if(dist < 20 && condictionOff){
+    irsend.sendNEC(0xE0E040BF,32);
+    condictionOn = true;
+    condictionOff = false;
+    delay(5000);
+  }
+
+  else if(dist > 100 && condictionOn){
+    irsend.sendNEC(0x2FD48B7, 32);
+    condictionOn = false;
+    condictionOff = true;
+    delay(7000);
+  }
+
+  delay(500);
+}
