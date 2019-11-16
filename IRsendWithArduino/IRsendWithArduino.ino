8fa284030e1016007f0c913a95257bbb32f5f81a
Gabriel <39504004+GitGabrielHub@users.noreply.github.com>
2019-11-16 11:19:10 -0300

    Update IRsendWithArduino.ino

--- IRsendWithArduino/IRsendWithArduino.ino
+++ IRsendWithArduino/IRsendWithArduino.ino
@@ -1,6 +1,11 @@
-#include <IRremote.h>
+//Utilização do sensor ultrassônico do Arduino para ligar/desligar TV
 
-IRsend irsend;
+#include <IRremote.h> //Biblioteca usada para que o led infravermelho (de um controle remoto IR qualquer) possa emitir pulsos 
+                      //correspondete a algum código binário específico que por conseguinte o receptor 
+                      //da TV vai decodificar e processar a tarefa exigida.
+
+IRsend irsend; //Por padrão da biblioteca, o led infravermelho deve estar no pino 3, ou seja, não precisa especificar
+               //o pinMode() no setup, a biblioteca faz por si só.
 
 byte PIN_ECHO = 6;
 byte PIN_TRIG = 5;
@@ -22,16 +27,17 @@ void setup(){
 
 void loop() {
 
-  digitalWrite(PIN_TRIG,HIGH);
+  digitalWrite(PIN_TRIG,HIGH);  
   delayMicroseconds(10);
   digitalWrite(PIN_TRIG,LOW);
 
   time = pulseIn(PIN_ECHO,HIGH);
 
-  dist = time/58;
+  dist = time/58; //Distância em centímetros.
 
-  if(dist < 20 && condictionOff){
-    irsend.sendNEC(0xE0E040BF,32);
+  if(dist < 20 && condictionOff){   //
+    irsend.sendNEC(0xE0E040BF,32);  //Chamada de função passada como primeiro parâmetro o 0x juntamente com o código hexadecimal que corresponde ao valor da tarefa
+                                    //ligar/desligar TV SAMSUNG, e como segundo parâmetro valor em bits correspondente a resolução.
     condictionOn = true;
     condictionOff = false;
     delay(5000);
